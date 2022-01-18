c9563811ed623b7a6b9590f95a9c6109782b7b56
Eugene Lee <scc135@gmail.com>
2022-01-18 08:53:57 -0500

    finished arn creation

--- server/controllers/s3Controller.js
+++ server/controllers/s3Controller.js
@@ -19,7 +19,7 @@ s3Controller.sendFile = (req, res, next) => {
   const fileStream = fs.createReadStream('out.zip');
   // console.log(fileStream);
   
-  const uploadParams = {
+  const params = {
     // s3 bucket
     Bucket: 'testbucketny30',
     // Add the required 'Key' parameter using the 'path' module.
@@ -28,18 +28,18 @@ s3Controller.sendFile = (req, res, next) => {
     Body: fileStream,
   };
 
-  s3Client.send(new PutObjectCommand(uploadParams))
+  s3Client.send(new PutObjectCommand(params))
     .then(data => console.log(data));
 };
 
 s3Controller.createBucket = (req, res, next) => {
-  const uploadParams = {
-    // s3 bucket
+  // params needed to create a s3 bucket
+  const params = {
+    // bucket name
     Bucket: 'testbucketny34',
-    
   };
   
-  s3Client.send(new CreateBucketCommand(uploadParams))
+  s3Client.send(new CreateBucketCommand(params))
     .then(data => {
       res.locals.data = data;
       next();
--- server/routers/awsRouter.js
+++ server/routers/awsRouter.js
@@ -4,6 +4,7 @@ import zip from '../controllers/util/zipUtility.js';
 import lambdaController from '../controllers/lambdaController.js';
 import s3Controller from '../controllers/s3Controller.js';
 import cloudWatchController from '../controllers/cloudWatchController.js';
+import iamController from '../controllers/iamController.js';
 
 // set up express router
 const router = express.Router();
@@ -15,17 +16,17 @@ router.use('/', (req, res, next) => {
 });
 
 // get a list of all lambda functions
-router.get('/funcList', lambdaController.getFuncList, (req, res, next) => {
+router.get('/funcList', lambdaController.getFuncList, (req, res) => {
   res.status(200).json(res.locals.functionList);
 });
 
 // invokes the command
-router.get('/command', lambdaController.invoke, (req, res, next) => {
+router.get('/command', lambdaController.invoke, (req, res) => {
   res.status(res.locals.lambdaResponse.statusCode).json(res.locals.lambdaResponse.body);
 });
 
 // creates a zip file from the lambda func
-router.get('/zipFile', (req, res, next) => {
+router.get('/zipFile', (req, res) => {
   console.log('      awsRouter.create zip file');
   // zip the file names given as strings. the first file is always turned to index
   res.locals.zipFileName = zip(['test.js']); 
@@ -33,22 +34,26 @@ router.get('/zipFile', (req, res, next) => {
 });
 
 // sends the file to S3 bucket. Not finished. Need to adjust parameters to accept different bucket name and file name
-router.get('/sendS3', s3Controller.sendFile, (req, res, next) => {
+router.get('/sendS3', s3Controller.sendFile, (req, res) => {
   res.status(200).json('done');
 });
 
 // sends the file to S3 bucket. Not finished. Need to adjust parameters to accept different bucket name and file name
-router.get('/createBucket', s3Controller.createBucket, (req, res, next) => {
+router.get('/createBucket', s3Controller.createBucket, (req, res) => {
   res.status(200).json(res.locals.data);
 });
 
-// sends the file to S3 bucket. Not finished. need to adjust parameters to accept diff bucket name, file name, function name
-router.get('/sendLambdaFunc', lambdaController.createFunction, (res, req, next) => {
+// sends the file from s3 to lambda. Not finished. need to adjust parameters to accept diff bucket name, file name, function name
+router.get('/sendLambdaFunc', lambdaController.createFunction, (res, req) => {
   res.status(200).json('done');
 });
 
+// creates an ARN. Not finished. need to adjust parameters to accept different role name
+router.get('/createARN', iamController.createRole, (req, res) => {
+  res.status(200).json('done');
+});
 // completely not working yet
-router.get('/getMetrics', cloudWatchController.getMetrics, (req, res, next) => {
+router.get('/getMetrics', cloudWatchController.getMetrics, (req, res) => {
   res.status(200).json('This is not a functioning route yet');
   
 });
