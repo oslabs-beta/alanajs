e98ed4e6da0911e87cfbd5c8511ab4807572cacb
tinhtarkhin <tinhtarkhin@gmail.com>
2022-01-20 15:33:59 -0500

    t

--- server/controllers/util/awsCredentials.js
+++ server/controllers/util/awsCredentials.js
@@ -4,12 +4,12 @@ dotenv.config();
 
 // root user credentials
 const credentials = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AKIARY42ZXW2KHZDLFPL,
+  secretAccessKey: process.env.XRDq99v6w8yAW1TqvvJN1z2aODnzYDxnbX6R5a,
 };
 
 // set up AWS region
-const region = process.env.AWS_REGION;
+const region = "us-east-1";
 
 const awsParams = {
   'region': region,
